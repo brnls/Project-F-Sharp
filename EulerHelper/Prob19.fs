e1ea5d11076f197755df4cdf799eb5773f50740e
Author Name <brnls1234@gmail.com>
2013-03-30 08:51:58 -0700

    Finished problem 19

---
+++ EulerHelper/Prob19.fs
@@ -0,0 +1,15 @@
+﻿module Prob19
+
+open EulerHelper.Util
+open EulerHelper
+open System.IO
+open System.Numerics
+open System
+
+let mutable date = new DateTime(1901,1,1)
+
+let mutable count = 0
+while date < new DateTime(2001,1,1) do
+    if date.Day = 1 && date.DayOfWeek = DayOfWeek.Sunday then
+        count <- count + 1
+    date <- date.AddDays(1.0)
